6026c62c707fd134cc1df4133611fe2a0f6b375d
Gerardo Vega <gerardoluisvega@gmail.com>
2025-04-20 17:26:02 -0300

    Se desarrollo la estructura de la pagina

---
+++ Landing_page_Delitos_Buenos_Aires/src/main.jsx
@@ -0,0 +1,5 @@
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+// import { Home } from "./routes/Home.jsx";
+createRoot(document.getElementById("root")).render(<App />);
